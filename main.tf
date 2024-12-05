a39d7f6c8f3da13831980fe707814f6ac279443b
christinanikolova <christinanikolova@github.com>
2024-12-05 11:29:38 +0200

    add storage

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.12.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstoragechrisi"
+    container_name = "taskboardscontainer"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
