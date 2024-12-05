f8057ab1c4f939af5eb903f10631af8bfbc3a6c1
christinanikolova <christinanikolova@github.com>
2024-12-05 12:11:03 +0200

    add create resourse storage account

--- main.tf
+++ main.tf
@@ -30,6 +30,13 @@ resource "azurerm_resource_group" "arg" {
   location = var.resource_group_location
 }
 
+resource "azurerm_storage_account" "asa" {
+  name                     = "StorageRG"
+  resource_group_name      = azurerm_resource_group.arg.name
+  location                 = azurerm_resource_group.arg.location
+  account_tier              = "Standard"
+  account_replication_type = "GRS"
+}
 resource "azurerm_service_plan" "asp" {
   name                = "${var.app_service_plan_name}${random_integer.ri.result}"
   location            = azurerm_resource_group.arg.location
